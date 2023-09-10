168e36e6d0b68b180d068d941ca8c753410d7a65
Noman Muhammad Nasir <102538287+mnomanmnasir@users.noreply.github.com>
2023-09-10 05:27:17 -0700

    working on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
-
+import HeroSlider from '../components/ProductCard/HeroSlider';
+import Helmet from '../components/Helmet/Helmet';
+import { Container, Row, Col } from 'reactstrap';
+import FindCarForm from '../components/ProductCard/FindCarForm';
+import '../styles/find-car-form.css'
 
 const Home = () => {
     return (
-<div>
-    Home
-</div>
+
+        <Helmet title='Home'>
+            {/* ---- home section -------- */}
+            <section className="p-0 hero_slider-section">
+                <HeroSlider />
+            </section>
+
+            <div className="hero_form">
+                <Container>
+                    <Row className="form_row">
+                        <Col lg='4' md='4'>
+                            <div className="find_cars-left">
+                                <h2>Find your best cars here</h2>
+                            </div>
+                        </Col>
+                        <Col lg='8' md='8' sm='12'>
+                            <FindCarForm />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+
+        </Helmet>
+
+
     )
 }
 
